332e93aee5e48e87bea464a19bf811417ca8778c
roshandhiman <roshanpreetsinghdhiman@gmail.com>
2026-08-10 22:51:58 +0530

    strings

---
+++ Strings/A.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class A {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String a=sc.next();
+        System.out.println(a);
+        System.out.println(a.length());
+        System.out.println(a.charAt(3));
+        System.out.println(a.toLowerCase());
+    }
+    
+}
