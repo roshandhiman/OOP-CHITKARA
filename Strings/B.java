04b08ad41a0255f4208e29a8ce2e317357715155
roshandhiman <roshanpreetsinghdhiman@gmail.com>
2026-08-10 23:46:10 +0530

    ahaha

---
+++ Strings/B.java
@@ -0,0 +1,9 @@
+public class B {
+    public static void main(String[] args) {
+        String a="Hello";
+        String b="HeLLo";
+        System.out.println(a.equals(b));
+        System.out.println(a.equalsIgnoreCase(b));
+    }
+    
+}
---
+++ Strings/C.java
@@ -0,0 +1,9 @@
+public class C {
+    public static void main(String[] args) {
+        String a="Programming";
+        System.out.println(a.substring(0,4));
+        String s = "banana";
+        System.out.println(s.replace('a', 'x'));
+    }
+    
+}
