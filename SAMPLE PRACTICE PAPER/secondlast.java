f72bfdd070bc7a23c9c5b62f743a15100c806004
roshandhiman <roshanpreetsinghdhiman@gmail.com>
2026-08-11 22:05:31 +0530

    all

---
+++ SAMPLE PRACTICE PAPER/secondlast.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class secondlast {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String a=sc.next();
+        StringBuilder sb=new StringBuilder(a);
+        System.out.println(sb.reverse());
+        
+    }
+    
+}
