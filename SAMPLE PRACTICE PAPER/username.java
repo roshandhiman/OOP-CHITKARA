9429aea1976d40d824005176d4148a3dca2f7d0d
roshandhiman <roshanpreetsinghdhiman@gmail.com>
2026-08-11 21:38:18 +0530

    12

---
+++ SAMPLE PRACTICE PAPER/username.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class username {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String a=sc.next();
+        String b=sc.next();
+        if(a.equals(b)){
+            System.out.println("Match");
+        }
+        else{
+            System.out.println("no match");
+        }
+    }
+    
+}
