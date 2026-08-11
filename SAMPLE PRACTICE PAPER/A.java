5f98d3975557f18ef9ac9dc1f5a0c9fb412d58d3
roshandhiman <roshanpreetsinghdhiman@gmail.com>
2026-08-11 14:46:13 +0530

    1st quesion

---
+++ SAMPLE PRACTICE PAPER/A.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class A {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String a=sc.nextLine();
+        a=a.toUpperCase();
+        String [] b=a.split(" ");
+        System.out.println(a);
+        System.out.println(b.length);
+
+    }
+    
+}
