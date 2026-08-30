2648e29a80b5136653ead95db6c616be0d38694d
roshandhiman <roshanpreetsinghdhiman@gmail.com>
2026-08-30 23:42:00 +0530

    Complete ArrayList C,I + add QUESTIONS.txt

--- CE3/ArrayList/C.java
+++ CE3/ArrayList/C.java
@@ -5,8 +5,17 @@ public class C {
         int n=sc.nextInt();
         ArrayList<Integer> a=new ArrayList<>();
         for(int i=0;i<n;i++){
-            
+            a.add(sc.nextInt());
         }
-        
+        int m=sc.nextInt();
+        ArrayList<Integer> b=new ArrayList<>();
+        for(int i=0;i<m;i++){
+            b.add(sc.nextInt());
+        }
+        ArrayList<Integer> c=new ArrayList<>();
+        c.addAll(a);
+        c.addAll(b);
+        Collections.sort(c);
+        System.out.println(c);
     }
 }
--- CE3/ArrayList/I.java
+++ CE3/ArrayList/I.java
@@ -1,7 +1,22 @@
 import java.util.*;
 public class I {
     public static void main(String[] args) {
-        
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        ArrayList<Integer> a=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            a.add(sc.nextInt());
+        }
+        ArrayList<Integer> even=new ArrayList<>();
+        ArrayList<Integer> odd=new ArrayList<>();
+        for(int num:a){
+            if(num%2==0) even.add(num);
+            else odd.add(num);
+        }
+        System.out.print("Even: ");
+        for(int num:even) System.out.print(num+" ");
+        System.out.println();
+        System.out.print("Odd: ");
+        for(int num:odd) System.out.print(num+" ");
     }
-    
 }
