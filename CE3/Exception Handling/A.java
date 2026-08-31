32763536b8aa0082f2f438390e0df6f82d7703c1
roshandhiman <roshanpreetsinghdhiman@gmail.com>
2026-08-31 21:16:19 +0530

    exceptionsss

---
+++ CE3/Exception Handling/A.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class A {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        try{
+            int result = a / b;
+            System.out.println(result);
+        } catch(ArithmeticException e) {
+            System.out.println("Cannot divide by zero");
+        }
+    }
+}
